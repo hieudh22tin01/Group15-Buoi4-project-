ad52bf921e1c59e4d0726599cd007a9652edf714
khoaaa204 <vankhoa100704@gmail.com>
2025-10-08 18:18:50 +0700

    V6

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
-// server.js
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // để đọc body JSON
-const userRoutes = require("./routes/user");
-
+app.use(express.json());
+const userRoutes = require("./routes/users");
 app.use("/users", userRoutes);
-
+app.get("/", (req, res) => {
+  res.send("Trang chủ API đang hoạt động 🚀");
+});
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
